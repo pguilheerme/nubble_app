d2b106bba9220013d61e0dec433960194a29a917
Pedro Guilherme <pedroguinasc2002@gmail.com>
2024-12-17 11:16:20 -0300

    feature: husky has been added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import {ThemeProvider} from '@shopify/restyle';
-import {theme} from './src/theme/theme';
-import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {SignUpScreen} from './src/screens/auth/SignUpScreen/SignUpScreen';
+
 import {Router} from './src/routes/Routes';
+import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
+import {SignUpScreen} from './src/screens/auth/SignUpScreen/SignUpScreen';
+import {theme} from './src/theme/theme';
 
 function App(): React.JSX.Element {
   return (
